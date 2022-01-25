2a7d997671207e92ac0a788803c1ab0e90203f81
Joaquín Yeray Martín de la Nuez <joaquinmn@protonmail.com>
2022-01-25 18:53:17 +0000

    create project

---
+++ src/test/java/com/jyeray/urlcutter/UrlCutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyeray.urlcutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCutterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
